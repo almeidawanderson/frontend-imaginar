ebd503e59cd5546bdf4926283141f90cb505b59d
Wanderson Almeida <wandersonalmeida.eng@gmail.com>
2023-06-03 22:01:25 -0300

    feat: add theme ligh and mui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,11 @@ import { Routes, Route, Navigate} from 'react-router-dom';
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='/pagina-inicial' element={<Button>Botão</Button>} />
+      <Route 
+        path='/pagina-inicial'
+        element={<Button
+        variant='contained'
+        color='primary'>Botão</Button>} />
 
       <Route path='*' element={<Navigate to={"/pagina-inicial"}/> } />
     </Routes>
